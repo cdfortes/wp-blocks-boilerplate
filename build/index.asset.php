6287fe170f776c11183d31c677df3555aeffa85b
cdfortes <cdfortes@gmail.com>
2022-04-20 16:46:23 -0100

    boilerplate complete to use

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'fd2891dbd18847d36df6695591c58a36');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '6ae64cb2c0c312728a7af1533777f0cd');
